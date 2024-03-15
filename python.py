31137a00dfebea5ae7aaaab086a186ee6efc89b2
صالح گلستانی <162115178+salehgolestani221181@users.noreply.github.com>
2024-03-15 20:59:50 +0330

    Add files via upload

---
+++ python.py
@@ -0,0 +1,12 @@
+Python 3.11.6 (tags/v3.11.6:8b6ee5b, Oct  2 2023, 14:57:12) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> p1="hellow python"
+>>> print (p1)
+hellow python
+>>> 
+>>> print (p1)
+hellow python
+>>> 
+>>> p2="welcom python"
+>>> retuen p2
+SyntaxError: incomplete input
